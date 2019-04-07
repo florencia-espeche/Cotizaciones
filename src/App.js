d02b6a55d45f801c3efa009967cf42a2a606f599
florencia <flor.espeche@gmail.com>
2019-04-07 18:29:18 -0300

    proyecto react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ContentCotizaciones from './Components/ContentCotizaciones';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>
+            Bolsa de valores
+          </h1>
         </header>
+        <body className="App-body">
+          <h1>
+            Contizaciones
+          </h1>
+          <ContentCotizaciones />
+        </body>
+        <footer className="App-footer">
+          <h1>
+            Florencia Espeche - Abril 2019
+          </h1>
+        </footer>
       </div>
     );
   }
